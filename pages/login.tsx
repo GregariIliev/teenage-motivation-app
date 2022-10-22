046babcbb4cf997d74fcf5310d1cbf8afee232bc
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-10-23 00:20:33 +0300

    create /login route page

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import Login from "../components/Login";
+
+const LoginPage: NextPage = () => {
+    return (
+        <Login />
+    )
+}
+
+export default LoginPage;
