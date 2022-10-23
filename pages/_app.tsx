41322eefd1d3c594a91bb4ea2dd743b874605a89
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-10-23 20:21:25 +0300

    set UserContex to chek if user is loggued

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,29 @@ import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import { UserContext } from '../context/userContext';
+import useUser from '../hooks/useUser';
 import Layout from '../components/Layout';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    user ? router.replace('/') : router.replace('/login');
+  }, [])
+  //[user]
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UserContext.Provider value={{ user }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserContext.Provider>
   )
 }
 
