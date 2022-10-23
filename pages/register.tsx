ce161488c0a54bad51a04614c10bf3a9b95f0faa
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-10-23 20:21:37 +0300

    create register page

---
+++ pages/register.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import Register from "../components/Register";
+
+const RegisterPage: NextPage = () => {
+    return <Register />
+}
+
+export default RegisterPage;
