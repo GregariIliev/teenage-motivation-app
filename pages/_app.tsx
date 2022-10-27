4899115e35b710210651eb0d9740910a0344ddaf
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-10-27 23:36:25 +0300

    remove useEffect and useRouter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,18 +7,9 @@ import { UserContext } from '../context/userContext';
 import useUser from '../hooks/useUser';
 import Layout from '../components/Layout';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
 function MyApp({ Component, pageProps }: AppProps) {
 
   const { user } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-   // user ? router.replace('/') : router.replace('/login');
-  }, [])
-  //[user]
 
   return (
     <UserContext.Provider value={{ user }}>
