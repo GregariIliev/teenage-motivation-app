ae15faed3b9d8aad5eeadedff1b729ae25c0d0ec
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-10-31 14:45:44 +0200

    comment firebase config app without active cloud fincctions

--- firebase/config.tsx
+++ firebase/config.tsx
@@ -7,13 +7,19 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCiVy6zo9xgySVL_k5bi5nVKMCa9pbQkwM",
-    authDomain: "teenage-motivation-app.firebaseapp.com",
-    projectId: "teenage-motivation-app",
-    storageBucket: "teenage-motivation-app.appspot.com",
-    messagingSenderId: "357481097255",
-    appId: "1:357481097255:web:7d1aa0eaa46a35b4428602",
-    measurementId: "G-WXFQZH4CYJ"
+    // apiKey: "AIzaSyCiVy6zo9xgySVL_k5bi5nVKMCa9pbQkwM",
+    // authDomain: "teenage-motivation-app.firebaseapp.com",
+    // projectId: "teenage-motivation-app",
+    // storageBucket: "teenage-motivation-app.appspot.com",
+    // messagingSenderId: "357481097255",
+    // appId: "1:357481097255:web:7d1aa0eaa46a35b4428602",
+    // measurementId: "G-WXFQZH4CYJ"
+    apiKey: "AIzaSyCGO-1LhBRV_XZfUndO6cf5iUgwDHLf3pA",
+    authDomain: "teen-app-d6280.firebaseapp.com",
+    projectId: "teen-app-d6280",
+    storageBucket: "teen-app-d6280.appspot.com",
+    messagingSenderId: "1080274298680",
+    appId: "1:1080274298680:web:9cd515820df2c835b983cd"
 };
 
 // Initialize Firebase
