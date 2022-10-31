c317e029c9d6ab400f529f877cadbcb436cb9da4
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-10-31 15:23:47 +0200

    make register only whit email and password

--- components/Register.tsx
+++ components/Register.tsx
@@ -7,36 +7,14 @@ export default function Register() {
     return (
         <form className="form">
             <div className="d-flex flex-column align-items-center justify-content-center">
-                <label htmlFor="nickname" className="form-label">Family Nickname</label>
-                <input type="text-sm" className="form-control form-control-sm w-25 mb-3" />
-                <label htmlFor="password" className="form-label">Family Password</label>
-                <input type="password" className="form-control form-control-sm w-25 mb-3" />
+                <label htmlFor="email" className="form-label">Email</label>
+                <input type="text-sm" className="form-control form-control-sm mb-3" />
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control form-control-sm mb-3" />
             </div>
-            <div className="row">
-                <div className="col">
-                    <label htmlFor="mother" className="form-label">Mother Name</label>
-                    <input type="text" className="form-control form-control-sm mb-3" />
-                </div>
-                <div className="col">
-                    <label htmlFor="father" className="form-label">Father Name</label>
-                    <input type="text" className="form-control form-control-sm mb-3" />
-                </div>
-                <div className="d-flex align-items-center justify-content-center">
-                    <input type="number" className="w-25" />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <label htmlFor="children" className="form-label">Children Name</label>
-                    <input type="text" className="form-control " />
-                </div>
-                <div className="col">
-                    <label htmlFor="children" className="form-label">Children Name</label>
-                    <input type="text" className="form-control" />
-                </div>
-            </div>
+
             <div className="container d-flex align-items-center justify-content-center w-25 ">
-                <button className="btn btn-primary mt-5" type="button">Regiser</button>
+                <button className="btn btn-primary mt-5" type="button">Register</button>
             </div>
         </form>
     )
