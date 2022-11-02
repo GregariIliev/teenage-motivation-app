c4a082e7318ecffb9874c963c43f581f7f952ddb
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-02 16:02:56 +0200

    remove all unnecessary imports

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,11 @@
 import { PropsWithChildren } from "react";
-import styles from '../styles/Layout.module.css';
-import Head from 'next/head';
-import Image from 'next/image';
-import Navbar from "./Navbar";
 
 export default function Layout({ children }: PropsWithChildren) {
 
 
     return (
         <>
-            <div className={styles.container}>
+            <div>
                 {children}
             </div>
         </>
