ea84e9cec468eee0cc16ec9d40947bd7db40964a
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-02 19:41:54 +0200

    rename classNames and create Links to login register and logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,30 +14,42 @@ const Navbar: NextPage = () => {
 
     const handleUserSingOut = (e: any) => {
         e.preventDefault();
+console.log('logout');
 
         user.userSingOut()
             .then((res: any) => { }).catch((err: any) => { })
     }
     return (
         <nav className={styles['navbar']}>
-            <ul className={styles['navbar-navigation']}>
-                <li className="li p-0 m-2 text-white">
+            <ul className={styles['navbar-ul-navigation']}>
+                <li className="li p-0 m-2">
                     <Link href='/tasks'>Today’s Tasks</Link>
                 </li>
-                <li className="li p-0 m-2 text-white">
+                <li className="li p-0 m-2">
                     <Link href='/done-tasks'>Done Tasks</Link>
                 </li>
-                <li className="li p-0 m-2 text-white">
+                <li className="li p-0 m-2">
                     <Link href='/calendar'>Calendar</Link>
                 </li>
-                <li className="li p-0 m-2 text-white">
+                <li className="li p-0 m-2">
                     <Link href='/members'>Members</Link>
                 </li>
-                <li className="li p-0 m-2 text-white">
+                <li className="li p-0 m-2">
                     <Link href='/points-collected'>Points Collected</Link>
                 </li>
             </ul>
-            <ul className={styles['navbar-username']}>
+            <ul className={styles['navbar-ul-navigation']}>
+                <li className="li p-0 m-2">
+                   <Link href={'/register'}>Register</Link>
+                </li>
+                <li className="li p-0 m-2">
+                   <Link href={'/login'}>Login</Link>
+                </li>
+                <li className="li p-0 m-2">
+                   <Link href={'/logout'}>Logout</Link>
+                </li>
+            </ul>
+            <ul className={styles['navbar-ul-username']}>
                 <li><span>Welcome Josh!</span></li>
             </ul>
         </nav>
