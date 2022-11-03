75f2a1f8aeacfdb617acf837e8c9c324b36b2899
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-03 13:51:49 +0200

    import router and redirect when user is logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 
 import Link from "next/link";
 
@@ -11,12 +12,17 @@ import styles from '../styles/Navbar.module.css';
 const Navbar: NextPage = () => {
 
     const user = useContext(UserContext);
+    const router = useRouter();
 
     const handleUserSingOut = (e: any) => {
         e.preventDefault();
 
         user.userSingOut()
-            .then((res: any) => { }).catch((err: any) => { })
+            .then((res: any) => {
+                router.replace('/login');
+            }).catch((err: any) => {
+                console.log(err);
+            })
     }
     return (
         <nav className={styles['navbar']}>
