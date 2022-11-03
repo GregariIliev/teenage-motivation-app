d6db969bac6d54811a6ed69fe7b5da3603ef5a61
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-03 13:48:24 +0200

    set name to inputs in the from

--- components/Login.tsx
+++ components/Login.tsx
@@ -13,11 +13,11 @@ export default function Login() {
             <form className="form">
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <input type="password" name="password" className="form-control" id="exampleInputPassword1" />
                 </div>
                 <div className="mb-3 form-check">
                     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
