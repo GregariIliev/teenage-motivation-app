2eb4c9de9aa1bd70566de8b0e8a51a91365a5e39
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-04 13:31:02 +0200

    set custom error and format code

--- components/Login.tsx
+++ components/Login.tsx
@@ -21,12 +21,13 @@ export default function Login() {
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                     <input type="password" name="password" className="form-control" id="exampleInputPassword1" required />
                 </div>
+                {error ? <p className='auth-error'>{error?.code.split('/')[1]}</p> : null}
                 {/* <div className="mb-3 form-check">
                     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                     <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
                 </div> */}
                 <div className="auth-buttons mt-4 w-100">
-                    <input className="btn w-100" type="submit" value="Login"/>
+                    <input className="btn w-100" type="submit" value="Login" />
                     <button onClick={authUser?.authUserWhitFacebook} className="btn w-100">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
                             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
@@ -35,9 +36,6 @@ export default function Login() {
                     </button>
                 </div>
                 <span className='mt-2'>Don’t have an account ?<span className='span-account'><Link href='/register'>Sing up</Link></span></span>
-                {user.error
-                    ? user.error
-                    : null}
             </form>
         </div>
     )
