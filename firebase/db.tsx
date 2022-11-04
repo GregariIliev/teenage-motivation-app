a494c7ed0cddbb25efb8c3d0425a6fb9e57e2fdc
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-04 22:23:16 +0200

    create a user collection whit the user template

--- firebase/db.tsx
+++ firebase/db.tsx
@@ -1,13 +1,15 @@
 import { app } from './config';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { userTemplate } from './templates/user';
+import { User } from 'firebase/auth';
 
 const db = getFirestore(app);
 
-export const addToCollection = async (collectionPath: string, item: any) => {
+export const addToCollection = async (collectionPath: string, user: User, fullName: string) => {
     try {
-        const docRef = await addDoc(collection(db, collectionPath), item);
-        console.log(docRef);
-        
+        const userT = userTemplate(user, fullName);
+
+        return await addDoc(collection(db, collectionPath), userT);
     } catch (err) {
         console.log(err);
         
