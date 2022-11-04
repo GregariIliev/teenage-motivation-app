54df9c5037351a939dbf8786ac7f4758ea29ff76
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-04 15:17:40 +0200

    create UserCollection type

--- types/userTypes.tsx
+++ types/userTypes.tsx
@@ -3,3 +3,13 @@ import { User } from "firebase/auth"
 export type UserAuth = {
     user: User | undefined
 }
+
+export type UserCollection = {
+    user: UserAuth,
+    fullName: string,
+    familyMembers: UserCollection[],
+    requestMembers: string | undefined,
+    currentTasks: [],
+    doneTasks:[],
+    points: number
+}
