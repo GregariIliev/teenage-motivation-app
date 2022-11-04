53c9f5fe7162b7389d7041cfad6d7239bbf05ff9
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-04 11:12:52 +0200

    set UserContext type UserTMP

--- context/userContext.tsx
+++ context/userContext.tsx
@@ -1,3 +1,4 @@
 import { createContext } from 'react';
+import { UserTMP } from '../types/userTypes';
 
-export const UserContext = createContext<any>({});
+export const UserContext = createContext<UserTMP>({user: undefined, authUser: undefined, error: undefined});
