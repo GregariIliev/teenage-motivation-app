74bd3fe959a699967ee2fad8077c9a990dcd4aab
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-04 13:31:25 +0200

    destructring error form UserContext

--- components/Register.tsx
+++ components/Register.tsx
@@ -5,7 +5,7 @@ import { UserContext } from "../context/userContext";
 
 export default function Register() {
 
-    const user = useContext(UserContext);
+    const { authUser, error } = useContext(UserContext);
 
     return (
         <div className="form-container">
