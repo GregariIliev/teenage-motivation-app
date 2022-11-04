de22cafe6eb03e410594c7ed2e454001a1b4e29f
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-04 22:20:58 +0200

    create a userTemplate

---
+++ firebase/templates/user.tsx
@@ -0,0 +1,17 @@
+import { User } from "firebase/auth";
+import { UserCollection } from "../../types/userInterface";
+
+export const userTemplate = (user: User, fullName: string) => {
+    const userT: UserCollection = {
+        userAuthId: user.uid,
+        fullName: fullName,
+        rol: "",
+        family: {},
+        requestMembers: [],
+        currentTasks: [],
+        doneTasks: [],
+        points: 0
+    }
+
+    return userT;
+}
