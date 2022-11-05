6a2a6b6ae71941f709ff23f49325ca309086c3d7
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-05 20:45:16 +0200

    create func to get user by id

--- firebase/db.tsx
+++ firebase/db.tsx
@@ -1,7 +1,8 @@
 import { app } from './config';
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getFirestore, collection, addDoc, query, getDocs, where } from 'firebase/firestore';
 import { userTemplate } from './templates/user';
 import { User } from 'firebase/auth';
+import { USER_COLLECTION_NAME } from './templates/user';
 
 const db = getFirestore(app);
 
@@ -10,6 +11,16 @@ export const addToCollection = async (collectionPath: string, user: User, fullNa
         const userT = userTemplate(user, fullName);
 
         return await addDoc(collection(db, collectionPath), userT);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export const getUserCollectionById = async (id: string) => {
+    try {
+        const q = query(collection(db, USER_COLLECTION_NAME), where("userAuthId", "==", id));
+
+        return await getDocs(q);
     } catch (err) {
         console.log(err);
     }
