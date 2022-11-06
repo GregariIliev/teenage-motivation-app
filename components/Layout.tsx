7085c0ca275b5a43bdd5255b3055fdb595462635
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-06 19:40:00 +0200

    conditional renderig Navar and Aside to login and registrer pages

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,24 @@
 import { PropsWithChildren } from "react";
+import { useRouter } from "next/router";
 import Aside from "./Aside";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }: PropsWithChildren) {
 
+    const router = useRouter();
 
     return (
         <>
             <header>
-                <Navbar />
+                {router.pathname === "/login" || router.pathname === "/register"
+                    ? null
+                    : <Navbar />}
             </header>
             <main className="main">
                 {children}
-                <Aside />
+                {router.pathname === "/login" || router.pathname === "/register"
+                    ? null
+                    : <Aside />}
             </main>
         </>
     )
