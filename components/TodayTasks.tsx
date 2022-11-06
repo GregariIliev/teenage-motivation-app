ef7a29485c231310c3c7ced27d862e3957d81e4f
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-11-06 20:09:38 +0200

    move Head from comonent to page

--- components/TodayTasks.tsx
+++ components/TodayTasks.tsx
@@ -5,15 +5,8 @@ import styles from '../styles/TodayTasks.module.css';
 
 export default function TodayTasks() {
     return (
-        <>
-            <Head>
-                <title>Today Tasks</title>
-                <meta name="today tasks" content="Today taks" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className={styles["today-tasks-container"]}>
-                <Tasks />
-            </div>
-        </>
+        <div className={styles["today-tasks-container"]}>
+            <Tasks />
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,22 @@ import type { NextPage } from 'next';
 import TodayTasks from '../components/TodayTasks';
 import { UserContext } from '../context/userContext';
 import { useContext } from 'react';
+import Head from 'next/head';
 
 const TodayTasksPage: NextPage = () => {
 
   const { user } = useContext(UserContext);
 
-  return user ? <TodayTasks /> : null;
+  return user ?
+    <>
+      <Head>
+        <title>Today Tasks</title>
+        <meta name="today tasks" content="Today taks" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <TodayTasks />
+    </>
+    : null;
 }
 
 export default TodayTasksPage;
